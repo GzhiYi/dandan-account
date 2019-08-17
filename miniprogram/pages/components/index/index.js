64847b94c974ee5ccb9617fbd12d537f76933b90
GzhiYi <gzy.guy@gmail.com>
2019-08-17 13:15:13 +0800

    增加输入判断

--- miniprogram/pages/components/index/index.js
+++ miniprogram/pages/components/index/index.js
@@ -11,13 +11,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    sum: ''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    bindInput(event) {
+      console.log(event)
+      const { value } = event.detail
+      this.setData({
+        sum: value
+      })
+    }
   }
 })

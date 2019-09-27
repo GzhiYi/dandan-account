69d6f70689ad8fcb616e057835baf0f01fc1d87e
GzhiYi <gzy.guy@gmail.com>
2019-09-27 23:37:14 +0800

    fix: word云函数，优化了环境的map配置。

--- cloudfunctions/word/index.js
+++ cloudfunctions/word/index.js
@@ -6,12 +6,13 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  const env = process.env ? (process.env.ENV === 'local' ? 'release-wifo3' : wxContext.ENV) : wxContext.ENV
   cloud.updateConfig({
-    env: process.env ? (process.env.ENV === 'local' ? 'release-wifo3' : wxContext.ENV) : wxContext.ENV
+    env
   })
   // 初始化数据库
   const db = cloud.database({
-    env: process.env ? (process.env.ENV === 'local' ? 'release-wifo3' : wxContext.ENV) : wxContext.ENV
+    env
   });
   const { mode } = event
   if (mode === 'get') {
@@ -42,7 +43,11 @@ exports.main = async (event, context) => {
           message: '无访问权限'
         }
       }
-      const res = await db.collection("DANDAN_WORD").doc('23fdfcbb-0f0c-4196-9d53-8c1ae616f04b')
+      const wordId = {
+        'release-wifo3': '23fdfcbb-0f0c-4196-9d53-8c1ae616f04b',
+        'dandan-zdm86': '9a27f33c-a75f-4495-a04d-0d5f98f51231'
+      }
+      const res = await db.collection("DANDAN_WORD").doc(wordId[env])
       .update({
         data: {
           word,

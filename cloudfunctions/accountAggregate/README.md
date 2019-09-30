9ba353412744efeb108625c88b160b5803cc476c
hqb <1025238479@qq.com>
2019-10-01 02:28:51 +0800

    获取饼图数据

--- cloudfunctions/accountAggregate/README.md
+++ cloudfunctions/accountAggregate/README.md
@@ -39,13 +39,12 @@
 
 ---
 
-### 根据FLOW流向详细聚合数据（函数名：accountAggregate, mode: aggregateAccountInDetail）
+### 获取饼图数据, mode: getPieChartData）
 
 | key| 说明 | 是否必填 |
 | -------- | ----- | ---- |
 | startDate|开始时间, 需要注意的是, 假如是2019-9-8日, 传入的格式必须是 ** 2019-09-08 **|是|
 | endDate|结束时间,  需要注意的是, 假如是2019-9-8日, 传入的格式必须是 ** 2019-09-08 **|是|
-| flow|金钱流向|是|
 
 ---
 返回格式:
@@ -54,15 +53,42 @@
 成功
 {
     code: 1,
-    detailResult: [
-        {
-            allSum: 152, // 当前菜单的账单的总额
-            count: 152, // 当前菜单的账单的条数
-            fatherCategoryId: "others",  // 当前菜单的菜单ID
-            fatherCategoryName: "杂项", // 当前菜单的菜单名字
-            sonCategories: ["others_sub", "a_cid", "b_cid"] // 当前菜单所包含的子菜单ID
+    detailResult: {
+        flowIn: {   // 流入
+            allSum: 100,    // 流入总金额
+            dataList: [
+                {
+                    categoryId: "others",
+                    categoryName: "杂项",
+                    allSum: 50,
+                    flow: 1
+                },
+                {
+                    categoryId: "b_others",
+                    categoryName: "另一个杂项",
+                    allSum: 50,
+                    flow: 1
+                },
+            ]
+        },
+        flowOut: {  // 流出
+            allSum: 30,    // 流出总金额
+            dataList: [
+                {
+                    categoryId: "out1",
+                    categoryName: "流出1",
+                    allSum: 10,
+                    flow: 0
+                },
+                {
+                    categoryId: "out2",
+                    categoryName: "流出1",
+                    allSum: 20,
+                    flow: 0
+                },
+            ]
         }
-    ]
+    }
 }
 
 
--- cloudfunctions/accountAggregate/index.js
+++ cloudfunctions/accountAggregate/index.js
@@ -20,30 +20,31 @@ exports.main = async (event, context) => {
   const $ = db.command.aggregate;
   const { mode, startDate, endDate, flow, categoryId, OPENID } = event;
 
-  // 按时间聚合, 聚合出支出和收入的数据
   try {
+    // 要显示的字段
+    const basicProject = {
+      _id: 0,
+      money: 1,
+      isDel: 1,
+      openId: 1,
+      flow: 1,
+      categoryId: 1,
+      isTarget: $.and([
+        $.gte([$.dateToString({
+          date: '$noteDate',
+          format: '%Y-%m-%d',
+          timezone: 'Asia/Shanghai'
+        }), startDate]),
+        $.lte([$.dateToString({
+          date: '$noteDate',
+          format: '%Y-%m-%d',
+          timezone: 'Asia/Shanghai'
+        }), endDate])
+      ])
+    };
+    
+  // 按时间聚合, 聚合出支出和收入的数据
     if (mode === 'aggregateAccountByDateRange') {
-      const basicProject = {
-        _id: 0,
-        money: 1,
-        isDel: 1,
-        openId: 1,
-        flow: 1,
-        categoryId: 1,
-        isTarget: $.and([
-          $.gte([$.dateToString({
-            date: '$noteDate',
-            format: '%Y-%m-%d',
-            timezone: 'Asia/Shanghai'
-          }), startDate]),
-          $.lte([$.dateToString({
-            date: '$noteDate',
-            format: '%Y-%m-%d',
-            timezone: 'Asia/Shanghai'
-          }), endDate])
-        ])
-      };
-
       const basicMatch = {
         isDel: false,
         openId: $.eq(OPENID ? OPENID : wxContext.OPENID),
@@ -63,164 +64,120 @@ exports.main = async (event, context) => {
       return {
         sumResult: sumResult.list.sort((a, b) => a._id - b._id)
       }
+    }
 
-      // 根据FLOW流向详细聚合数据
-      // if (mode === 'aggregateAccountInDetail') {
+    // 获取饼图数据
+    if (mode === 'getPieChartData') {
+      const basicMatch = {
+        isDel: false,
+        openId: $.eq(OPENID ? OPENID : wxContext.OPENID),
+        isTarget: true,
+      };
 
-      //   const basicProject = {
-      //     _id: 0,
-      //     money: 1,
-      //     isDel: 1,
-      //     openId: 1,
-      //     flow: 1,
-      //     categoryId: 1,
-      //     isTarget: $.and([
-      //       $.gte([$.dateToString({
-      //         date: '$noteDate',
-      //         format: '%Y-%m-%d',
-      //         timezone: 'Asia/Shanghai'
-      //       }), startDate]),
-      //       $.lte([$.dateToString({
-      //         date: '$noteDate',
-      //         format: '%Y-%m-%d',
-      //         timezone: 'Asia/Shanghai'
-      //       }), endDate])
-      //     ])
-      //   };
-
-      //   const basicMatch = {
-      //     isDel: false,
-      //     openId: $.eq(OPENID ? OPENID : wxContext.OPENID),
-      //     flow: Number(flow),
-      //     isTarget: true,
-      //   };
-
-      //   // 求详细的结果, 只涉及到二级目录
-      //   const detailResult = await db.collection("DANDAN_NOTE")
-      //     .aggregate()
-      //     .project(basicProject)
-      //     .match(basicMatch)
-      //     .group({
-      //       _id: '$categoryId',
-      //       allSum: $.sum("$money"),
-      //       count: $.sum(1),
-      //     })
-      //     .end();
-
-      //   return {
-      //     code: 1,
-      //     detailResult: detailResult.list,
-      //   }
-      // }
-
-      // 根据FLOW流向详细聚合数据
-      if (mode === 'aggregateAccountInDetail') {
-
-        const basicProject = {
+      const detailResult = await db.collection("DANDAN_NOTE")
+        .aggregate()
+        .project(basicProject)
+        .match(basicMatch)
+        .group({
+          _id: {
+            categoryId: '$categoryId',
+            flow: '$flow'
+          },
+          allSum: $.sum("$money"),
+          count: $.sum(1),
+        })
+        .replaceRoot({
+          newRoot: $.mergeObjects(["$_id", '$$ROOT'])
+        })
+        .project({
+          _id: 0
+        })
+        // 查子目录的信息, 以此获取父目录的ID
+        .lookup({
+          from: 'DANDAN_NOTE_CATEGORY',
+          localField: 'categoryId',
+          foreignField: '_id',
+          as: 'categoryInfo',
+        })
+        .replaceRoot({
+          newRoot: $.mergeObjects([$.arrayElemAt(['$categoryInfo', 0]), '$$ROOT'])
+        })
+        .project({
+          allSum: 1,
+          count: 1,
+          flow: 1,
           _id: 0,
-          money: 1,
-          isDel: 1,
-          openId: 1,
+          fatherCategoryId: '$parentId',
+        })
+        // 已经得到parentId, 可以再次进行聚合
+        .group({
+          _id: {
+            categoryId: '$fatherCategoryId',
+            flow: "$flow"
+          },
+          allSum: $.sum("$allSum"),
+          count: $.sum("$count"),
+        })
+        .replaceRoot({
+          newRoot: $.mergeObjects(["$_id", '$$ROOT'])
+        })
+        .project({
+          _id: 0
+        })
+        // 用父目录ID查询父目录信息
+        .lookup({
+          from: 'DANDAN_NOTE_CATEGORY',
+          localField: 'categoryId',
+          foreignField: '_id',
+          as: 'fatherCategoryInfo',
+        })
+        .replaceRoot({
+          newRoot: $.mergeObjects([$.arrayElemAt(['$fatherCategoryInfo', 0]), '$$ROOT'])
+        })
+        .project({
+          _id: 0,
+          allSum: 1,
+          count: 1,
           flow: 1,
           categoryId: 1,
-          isTarget: $.and([
-            $.gte([$.dateToString({
-              date: '$noteDate',
-              format: '%Y-%m-%d',
-              timezone: 'Asia/Shanghai'
-            }), startDate]),
-            $.lte([$.dateToString({
-              date: '$noteDate',
-              format: '%Y-%m-%d',
-              timezone: 'Asia/Shanghai'
-            }), endDate])
-          ])
-        };
+          categoryName: 1
+        })
+        .end();
+      
+      const returnObj = {}
 
-        const basicMatch = {
-          isDel: false,
-          openId: $.eq(OPENID ? OPENID : wxContext.OPENID),
-          flow: Number(flow),
-          isTarget: true,
-        };
-
-        const detailResult = await db.collection("DANDAN_NOTE")
-          .aggregate()
-          .project(basicProject)
-          .match(basicMatch)
-          .group({
-            _id: '$categoryId',
-            allSum: $.sum("$money"),
-            count: $.sum(1),
-          })
-          // 查子目录的信息, 以此获取父目录的ID
-          .lookup({
-            from: 'DANDAN_NOTE_CATEGORY',
-            localField: '_id',
-            foreignField: '_id',
-            as: 'categoryInfo',
-          })
-          .replaceRoot({
-            newRoot: $.mergeObjects([$.arrayElemAt(['$categoryInfo', 0]), '$$ROOT'])
-          })
-          .project({
-            allSum: 1,
-            count: 1,
-            flow: 1,
-            _id: 0,
-            sonCategoryName: '$categoryName',
-            fatherCategoryId: '$parentId',
-            sonCategoryId: '$_id'
-          })
-          // 用父目录ID查询父目录信息
-          .lookup({
-            from: 'DANDAN_NOTE_CATEGORY',
-            localField: 'fatherCategoryId',
-            foreignField: '_id',
-            as: 'fatherCategoryInfo',
-          })
-          .replaceRoot({
-            newRoot: $.mergeObjects([$.arrayElemAt(['$fatherCategoryInfo', 0]), '$$ROOT'])
-          })
-          .project({
-            _id: 0,
-            allSum: 1,
-            count: 1,
-            flow: 1,
-            sonCategoryName: 1,
-            sonCategoryId: 1,
-            fatherCategoryId: 1,
-            fatherCategoryName: '$categoryName',
-          })
-          // 最后把数据聚合到父目录
-          .group({
-            _id: '$fatherCategoryId',
-            fatherCategoryName: $.first("$fatherCategoryName"),
-            allSum: $.sum("$allSum"),
-            count: $.sum("$count"),
-            sonCategories: $.addToSet("$sonCategoryId"),
-          })
-          .project({
-            _id: 0,
-            fatherCategoryId: '$_id',
-            fatherCategoryName: 1,
-            allSum: 1,
-            count: 1,
-            flow: 1,
-            sonCategories: 1,
-          })
-          .end();
-        return {
-          code: 1,
-          detailResult: detailResult.list,
+      const flowOutList = [];
+      const flowInList = [];
+      let sumAllIn = 0;
+      let sumAllOut = 0;
+      // 遍历获取每个流的总金额
+      for (let item of detailResult.list) {
+        if (item.flow === 1) {
+          sumAllIn = keepTwoDecimal(sumAllIn + item.allSum)
+          flowInList.push(item)
+        } else {
+          sumAllOut = keepTwoDecimal(sumAllOut + item.allSum)
+          flowOutList.push(item)
         }
+      }
+      returnObj.flowIn = {
+        allSum: sumAllIn,
+        dataList: flowInList
+      }
+      returnObj.flowOut = {
+        allSum: sumAllOut,
+        dataList: flowOutList
+      }
+
+      return {
+        code: 1,
+        detailResult: returnObj,
       }
     }
   } catch (e) {
     console.error(e)
     return {
-      sumResult: sumResult.list.length > 0 ? sumResult.list[0].sort((a, b) => a._id - b._id) : {},
-      detailResult: detailResult.list,
+      code: 0
     }
   }
 }

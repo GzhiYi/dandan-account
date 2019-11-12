039c641e363d3c2b12bc7172eba982bf805c8c72
GzhiYi <gzy.guy@gmail.com>
2019-11-12 20:39:03 +0800

    feat: 优化折线图样式

--- miniprogram/pages/morechart/morechart.js
+++ miniprogram/pages/morechart/morechart.js
@@ -1,9 +1,12 @@
 import uCharts from '../u-charts'
-let canvasPie = null
+import { parseTime } from '../../util'
+let lineChartA = null
+let lineChartB = null
 Page({
   data: {
     cWidth: 0,
-    cHeight: 0
+    cHeight: 0,
+    date: parseTime(new Date(), '{y}-{m}')
   },
   onLoad: function (options) {
     // TODO, It is fake chart in testing.
@@ -15,7 +18,7 @@ Page({
       cWidth,
       cHeight
     } = this.data
-    canvasPie = new uCharts({
+    lineChartA = new uCharts({
       $this: self,
       canvasId: 'linechart-month',
       type: 'line',
@@ -88,16 +91,130 @@ Page({
         max: 180,
         format: (val) => { return val.toFixed(0) + '元' }//如不写此方法，Y轴刻度默认保留两位小数
       },
-      width: 300,
+      width: 385,
       height: 200,
       extra: {
         line: {
-          type: 'straight'
+          type: 'curve'
+        }
+      },
+    })
+    lineChartB = new uCharts({
+      $this: self,
+      canvasId: 'linechart-year',
+      type: 'line',
+      fontSize: 11,
+      legend: true,
+      dataLabel: true,
+      dataPointShape: true,
+      background: '#FFFFFF',
+      pixelRatio: 1,
+      categories: [
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017"
+      ],
+      series: [
+        {
+          "name": "成交量A",
+          "data": [
+            35,
+            8,
+            25,
+            37,
+            4,
+            20
+          ]
+        },
+        {
+          "name": "成交量B",
+          "data": [
+            70,
+            40,
+            65,
+            100,
+            44,
+            68
+          ]
+        },
+        {
+          "name": "成交量C",
+          "data": [
+            100,
+            80,
+            95,
+            150,
+            112,
+            132
+          ]
+        }
+      ],
+      animation: true,
+      enableScroll: true,//开启图表拖拽功能
+      xAxis: {
+        disableGrid: false,
+        type: 'grid',
+        gridType: 'dash',
+        itemCount: 4,
+        scrollShow: true,
+        scrollAlign: 'left',
+        //scrollBackgroundColor:'#F7F7FF',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条背景颜色,默认为 #EFEBEF
+        //scrollColor:'#DEE7F7',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条颜色,默认为 #A6A6A6
+      },
+      yAxis: {
+        //disabled:true
+        gridType: 'dash',
+        splitNumber: 8,
+        min: 10,
+        max: 180,
+        format: (val) => { return val.toFixed(0) + '元' }//如不写此方法，Y轴刻度默认保留两位小数
+      },
+      width: 385,
+      height: 200,
+      extra: {
+        line: {
+          type: 'curve'
         }
       },
     })
   },
-  touchPie(event) {
+  touchLineA(e) {
+    lineChartA.scrollStart(e);
+  },
+  moveLineA(e) {
+    lineChartA.scroll(e);
+  },
+  touchEndLineA(e) {
+    lineChartA.scrollEnd(e);
+    //下面是toolTip事件，如果滚动后不需要显示，可不填写
+    lineChartA.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  touchLineB(e) {
+    lineChartB.scrollStart(e);
+  },
+  moveLineB(e) {
+    lineChartB.scroll(e);
+  },
+  touchEndLineB(e) {
+    lineChartB.scrollEnd(e);
+    //下面是toolTip事件，如果滚动后不需要显示，可不填写
+    lineChartB.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  bindDateChange(event) {
     console.log('event', event)
+    this.setData({
+      date: event.detail.value
+    })
   }
 })

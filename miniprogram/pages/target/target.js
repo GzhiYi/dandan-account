f361cf1a7a94bbbbb0ff5f235c1d1b7f29aa8500
GzhiYi <zhiyi.gong@outlook.com>
2021-12-23 23:59:30 +0800

    fix: 移除ucharts

--- miniprogram/pages/target/target.js
+++ miniprogram/pages/target/target.js
@@ -1,11 +1,8 @@
-import uCharts from '../u-charts'
-import { parseTime, strip } from '../../util'
+import { strip } from '../../util'
 import dayjs from 'dayjs'
 // import cloneDeep from 'lodash/cloneDeep'
 const { importStore } = getApp()
 const { create, store } = importStore
-// eslint-disable-next-line no-unused-vars
-let lineChart = null
 const baseProgressConfig = (chart, target, text) => {
   chart.axis(false);
   chart.tooltip(false);
@@ -119,14 +116,14 @@ create.Page(store, {
     const self = this
     // 处理账单，按每天进行保存
     const formatBillList = {}
-    const formatAllDate = allDate.map((item) => parseTime(item, '{y}-{m}-{d}'))
-    const lastBillDate = parseTime(billList[billList.length - 1].noteDate, '{y}-{m}-{d}')
+    const formatAllDate = allDate.map((item) => dayjs(item).format('YYYY-MM-DD'))
+    const lastBillDate = dayjs(billList[billList.length - 1].noteDate).format('YYYY-MM-DD')
     const lastBillDateIndex = formatAllDate.indexOf(lastBillDate)
     let nowMoney = 0
     billList.forEach((bill) => {
       formatAllDate.forEach((day, index) => {
         if (index <= lastBillDateIndex) {
-          if (day === parseTime(bill.noteDate, '{y}-{m}-{d}')) {
+          if (day === dayjs(bill.noteDate).format('YYYY-MM-DD')) {
             if (!formatBillList[day]) formatBillList[day] = 0
             const setMoney = bill.flow === 0 ? formatBillList[day] -= bill.money : formatBillList[day] += bill.money
             // eslint-disable-next-line no-restricted-globals
@@ -246,83 +243,6 @@ create.Page(store, {
         lineChartShow: true
       })
     })
-    lineChart = new uCharts({
-      $this: self,
-      canvasId: 'linechart',
-      type: 'area',
-      fontSize: 11,
-      legend: {
-        show: false
-      },
-      dataLabel: false,
-      dataPointShape: false,
-      enableMarkLine: true,
-      background: 'rgba(255, 255, 255, 0)',
-      pixelRatio: 1,
-      categories: keys,
-      padding: [23, 0, 20, 0],
-      series: [
-        {
-          color: '#E9EBF3',
-          data: seriesData,
-          index: 0,
-          legendShape: 'line',
-          name: '',
-          pointShape: 'circle',
-          show: true,
-          type: 'line',
-          addPoint: true
-        }
-      ],
-      animation: true,
-      xAxis: {
-        disableGrid: true,
-        type: 'grid',
-        gridType: 'dash',
-        scrollShow: false,
-        scrollAlign: 'left',
-        disabled: true,
-        gridColor: '#3F4D8D',
-        axisLineColor: '#3F4D8D'
-        // scrollBackgroundColor:'#F7F7FF',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条背景颜色,默认为 #EFEBEF
-        // scrollColor:'#DEE7F7',//可不填写，配合enableScroll图表拖拽功能使用，X轴滚动条颜色,默认为 #A6A6A6
-      },
-      yAxis: {
-        // disabled:true
-        gridType: 'solid',
-        dashLength: 1,
-        gridColor: '#3F4D8D',
-        data: [{
-          axisLineColor: '#3F4D8D',
-          fontColor: '#7D87B5',
-          max: targetData.targetMoney,
-          min: 0
-        }],
-        splitNumber: 5
-      },
-      width: store.data.sysInfo.screenWidth * 0.8,
-      height: 200,
-      extra: {
-        line: {
-          type: 'curve'
-        },
-        area: {
-          opacity: 0.3,
-          gradient: true,
-          addLine: true
-        },
-        markLine: {
-          data: [{
-            value: targetData.targetMoney,
-            color: '#4fd69c',
-            lineColor: '#4fd69c',
-            dashLength: 1
-          }],
-          type: 'dash'
-        }
-      }
-    })
-    // eslint-disable-next-line no-console
   },
   handlePercentage(percentage) {
     if (percentage >= 1) {
@@ -381,40 +301,6 @@ create.Page(store, {
         })
       })
     }
-    const self = this
-    // eslint-disable-next-line no-new
-    new uCharts({
-      $this: self,
-      canvasId: data.id,
-      type: 'arcbar',
-      fontSize: 22,
-      dataLabel: true,
-      background: 'rgba(255, 255, 255, 0)',
-      pixelRatio: 1,
-      animation: true,
-      series: [{
-        data: data.percentage,
-        color: '#fff'
-      }],
-      title: {
-        name: `${data.percentage * 100}%`,
-        color: '#fff',
-        fontSize: 25
-      },
-      subtitle: {
-        name: data.subTitle,
-        color: '#fff',
-        fontSize: 15
-      },
-      padding: [0, 0, 0, 0],
-      width: store.data.sysInfo.screenWidth * 0.3,
-      height: 120,
-      extra: {
-        arcbar: {
-          backgroundColor: data.bgColor
-        }
-      }
-    })
   },
   getDates(startDate, endDate) {
     const dates = []

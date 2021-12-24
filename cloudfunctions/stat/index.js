6a803a3775e0c98216c78a8754dcaed09a940ece
GzhiYi <gzy.guy@gmail.com>
2021-12-24 16:58:34 +0800

    feat: add daily stat.

--- cloudfunctions/stat/index.js
+++ cloudfunctions/stat/index.js
@@ -3,28 +3,43 @@
 1. 日期
 2. 支出
 3. 收入
-4. 净资产
+4. 净收入
 5. 记账次数
 6. 用户openId
  */
 const cloud = require('wx-server-sdk')
+const dayjs = require('dayjs')
 
 cloud.init()
 const MAX_LIMIT = 100
-
+function strip(num, precision = 12) {
+  return +parseFloat(num.toPrecision(precision))
+}
 // 云函数入口函数
-exports.main = async () => {
+exports.main = async (event) => {
   const wxContext = cloud.getWXContext()
-  cloud.updateConfig({
-    env: wxContext.ENV === 'local' ? 'release-wifo3' : wxContext.ENV
-  })
+  const env = wxContext.ENV === 'local' ? 'dandan-zdm86' : wxContext.ENV
+  cloud.updateConfig({ env })
   // 初始化数据库
-  const db = cloud.database({
-    env: wxContext.ENV === 'local' ? 'release-wifo3' : wxContext.ENV
-  })
+  const db = cloud.database({ env })
   const _ = db.command
+  // 有openId则表示只更新某个人的统计记录
+  const { openId, noteDate } = event
+  // 传入日期的开始和结束时间
+  // 由于是定时任务，所以需要减少一天
+  const todayStr = dayjs(noteDate).subtract(1, 'day').format('YYYY-MM-DD')
+  const startTime = `${todayStr} 00:00:00`
+  const endTime = `${todayStr} 23:59:59`
+  if (!noteDate) {
+    return {
+      code: 0,
+      msg: '未传入日期',
+      data: null
+    }
+  }
+  // 由于性能有限，现在默认只统计自然日的数据，后面再把其他数据跑回来
   const queryAll = async (collectName, queryParams) => {
-    const resultNum = await db.collection(collectName).count()
+    const resultNum = await db.collection(collectName).where(queryParams).count()
     const { total } = resultNum
     const batchTimes = Math.ceil(total / MAX_LIMIT)
     const tasks = []
@@ -40,61 +55,67 @@ exports.main = async () => {
       errMsg: acc.errMsg
     }))
   }
-  const calNote = (noteList) => {
-    if (!noteList) {
-      return {
-        pay: 0,
-        income: 0,
-        netIncome: 0,
-        noteLength: 0
-      }
-    }
-    let pay = 0
-    let income = 0
-    noteList.forEach((note) => {
-      if (note.flow === 0) {
-        pay += note.money
-      }
-      if (note.flow === 1) {
-        income += note.money
-      }
-    })
-    return {
-      pay,
-      income,
-      netIncome: income - pay,
-      noteTime: noteList.length
-    }
-  }
-  const userData = await queryAll('USERS')
-  const userList = userData.data
-  const endDate = new Date()
-  const startDate = new Date().setDate(new Date().getDate() - 1)
-  if (userList instanceof Array) {
-    for (let i = 0; i < userList.length; i++) {
-      // eslint-disable-next-line no-await-in-loop
-      const noteData = await queryAll('DANDAN_NOTE', {
-        openId: userList[i].openId,
-        noteDate: _.gt(new Date(startDate)).and(_.lte(endDate)),
-        isDel: false
+  const calNote = (noteList, openIdList) => {
+    let index = -1
+    const addData = []
+    while (++index < openIdList.length) {
+      const oneOpenId = openIdList[index]
+      const noteListByOpenId = noteList.filter((note) => note.openId === oneOpenId)
+      let pay = 0
+      let income = 0
+      let netAsset = 0
+      let payCount = 0
+      let incomeCount = 0
+      noteListByOpenId.forEach((note) => {
+        // 支出
+        if (note.flow === 0) {
+          pay += note.money
+          payCount += 1
+        } else {
+          income += note.money
+          incomeCount += 1
+        }
       })
-      if (noteData.data && noteData.data.length) {
-        const {
-          pay, income, netIncome, noteTime
-        } = calNote(noteData.data)
-        // eslint-disable-next-line no-await-in-loop
-        await db.collection('STAT').add({
-          data: {
-            date: new Date(),
-            pay,
-            income,
-            netIncome,
-            noteTime,
-            openId: wxContext.OPENID
-          }
-        })
-      }
+      netAsset = income - pay
+      addData.push({
+        openId: oneOpenId,
+        noteDate,
+        pay: strip(pay),
+        income: strip(income),
+        netAsset: strip(netAsset),
+        payCount,
+        incomeCount
+      })
     }
+    return addData
+  }
+  const params = {
+    noteDate: _.gte(new Date(startTime)).and(_.lte(new Date(endTime))),
+    isDel: false
+  }
+  if (openId) {
+    params.openId = openId
+  }
+  const noteListRes = await queryAll('DANDAN_NOTE', params)
+  const noteList = noteListRes.data
+  // get openId from noteList
+  const openIdList = Array.from(new Set(noteList.map((note) => note.openId)))
+  const addData = calNote(noteList, openIdList)
+  // 有openId，则只更新该用户该天的统计数据
+  if (openId) {
+    const oldRes = await db.collection('STAT').where({
+      openId,
+      noteDate: _.eq(todayStr)
+    }).get()
+    // 更新该条记录
+    await db.collection('STAT').doc(oldRes.data[0]._id).update({
+      data: addData[0]
+    })
+  } else {
+    // 插入今日统计数据
+    await db.collection('STAT').add({
+      data: addData
+    })
   }
   return {
     code: 1,
